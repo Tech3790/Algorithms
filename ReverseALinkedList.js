dbdc72325c573d2d9c859449a9ffb4342aa1fccd
Abdullah Alabasi <Tech3790@gmail.com>
2019-02-27 15:12:02 -0600

    added the recursive way to reverse a linked list

--- ReverseALinkedList.js
+++ ReverseALinkedList.js
@@ -2,12 +2,13 @@
 An algorithm to reverse a given linked list using two different approaches:
 */
 
-// Approach #1 the iterative way using a while loop we iterate through
+// Approach #1 the iterative way using a while loop we iterate through.
 // the linked list and reverse the links of each node to the previous one.
+// Linear time complexity is O(n).
 const reverseLinkedList = head => {
   let prev = (next = null);
   let current = head;
-// if there is only one node in the given list we return that node.
+  // if there is only one node in the given list we return that node.
   if (!current.next) return current;
   while (current) {
     next = current.next;
@@ -16,4 +17,15 @@ const reverseLinkedList = head => {
     current = next;
   }
   return prev;
+};
+// Approach #2 using recursion.
+// Linear time complexity is O(n).
+const reverseLinkedList = (node, parent) => {
+  let result = parent || {};
+  if (node) {
+    const child = node.next;
+    node.next = parent;
+    result = reverse(child, node);
+  }
+  return result;
 };
